312d604169669d411a7112e82d3b1536f2ff191d
Joe Baranski <joe.baranski49@gmail.com>
2020-12-11 13:30:35 -0500

    added crossorigin annotation

--- service/src/main/java/com/jump/controller/CustomerController.java
+++ service/src/main/java/com/jump/controller/CustomerController.java
@@ -8,6 +8,7 @@ import com.jump.service.CustomerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,25 +19,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/")
+@RequestMapping("customers")
 public class CustomerController {
 
   @Autowired
   CustomerService customerService;
 
-  @GetMapping("customers")
+  @GetMapping()
   public ResponseEntity<List<Customer>> getCustomers() {
 
     return ResponseEntity.ok(customerService.getCustomers());
   }
 
-  @GetMapping("customers/{id}")
+  @GetMapping("/{id}")
   public Customer getCustomer(@PathVariable("id") int id) {
     return customerService.getCustomerById(id);
   }
 
-  @PostMapping("customers")
+  @PostMapping()
   public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) {
 
     Customer result = customerService.addCustomer(customer);
@@ -47,16 +49,16 @@ public class CustomerController {
 
   }
 
-  @PutMapping("customers/{customerId}")
-  public Customer updateCustomer(@PathVariable Integer customerId, @RequestBody Customer customer) {
+  @PutMapping("/{customerId}")
+  public Customer updateCustomer(@PathVariable int customerId, @RequestBody Customer customer) {
 
     Customer result = customerService.updateCustomer(customer);
 
     return result;
   }
 
-  @DeleteMapping("customers/{customerId}")
-  public ResponseEntity<?> deleteCustomer(@PathVariable Integer customerId) {
+  @DeleteMapping("/{customerId}")
+  public ResponseEntity<?> deleteCustomer(@PathVariable int customerId) {
     customerService.deleteCustomer(customerId);
     return ResponseEntity.noContent().build();
   }
